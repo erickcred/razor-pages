dc1cb45bb11202d423d81255a3173946d5b620a5
erickcred <erickcredjes@gmail.com>
2023-08-26 10:20:08 -0300

    Configurações iniciais

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+/* Configurações para tonar a aplicação acessivel via Browser */
+app.UseHttpsRedirection();
+app.UseStaticFiles(); // utiliza o (wwwroot) css, js, images etc..
+
+// Auxiliar no mapeamento das paginas
+app.UseRouting();
+app.MapRazorPages();
 
 app.Run();
