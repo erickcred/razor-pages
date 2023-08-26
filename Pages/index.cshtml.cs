b5854a62d55a49236a94bcc8319530f022e2f947
erickcred <erickcredjes@gmail.com>
2023-08-26 14:10:02 -0300

    Criando Páginas About e Login

--- Pages/index.cshtml.cs
+++ Pages/index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -10,15 +11,27 @@ namespace MyRazorApp.Pages
 {
     public class Index : PageModel
     {
-        private readonly ILogger<Index> _logger;
+        public List<Category> Categories { get; set; } = new();
 
-        public Index(ILogger<Index> logger)
+        public async Task OnGet()
         {
-            _logger = logger;
+            
+            // await Task.Delay(5000);
+            for (int i = 0; i <= 100; i++)
+            {
+                this.Categories.Add(new Category(i, $"Categoria {i}", (i * 2.95M)));
+            }
         }
 
-        public void OnGet()
+        public void OnPost()
         {
+
         }
     }
 }
+
+public record Category(
+    int Id,
+    string Title,
+    decimal Price
+);
